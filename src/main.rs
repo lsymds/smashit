aa289d80138b040f12c9abb5c5e069b6f3ba9924
Liam Symonds <liam@liamsymonds.com>
2022-11-09 22:20:09 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::ops::Add;
+
+fn main() {
+    if let Some(parsed_args) = parse_args(std::env::args().collect()) {
+        if parsed_args.url.is_empty() {
+            show_help();
+            return;
+        }
+
+        println!(
+            "URL is: {0} - Method is: {1} - Count is {2}",
+            parsed_args.url, parsed_args.method, parsed_args.count
+        );
+    } else {
+        show_help();
+    }
+}
+
+fn parse_args(args: Vec<String>) -> Option<ParsedArgs> {
+    let mut path = String::from("");
+    let mut method = String::from("GET");
+    let mut count = 1;
+
+    let mut iterator = 1;
+    while iterator < args.len() {
+        match args[iterator].as_str() {
+            "-u" | "--url" => path = get_next_argument(&mut iterator, &args)?,
+            "-m" | "--method" => method = get_next_argument(&mut iterator, &args)?,
+            "-c" | "--count" => {
+                count = get_next_argument(&mut iterator, &args).and_then(|s| s.parse().ok())?
+            }
+            _ => return None,
+        }
+    }
+
+    return Some(ParsedArgs {
+        url: path,
+        method,
+        count,
+    });
+}
+
+fn get_next_argument(current_position: &mut usize, args: &Vec<String>) -> Option<String> {
+    if args.len() - 1 < *current_position + 1 || args[*current_position + 1].is_empty() {
+        None
+    } else {
+        *current_position = current_position.add(2);
+        Some(args[*current_position - 1].clone())
+    }
+}
+
+fn show_help() {
+    println!(
+        "
+smashit - simple single machine load testing tool built to learn Rust
+
+usage: smashit [options]
+
+options:
+  -c | --count  The number of times to call the endpoint (default: 1)
+  -u | --url    The URL to load test
+  -m | --method The HTTP method to use in the request (default: GET)"
+    );
+}
+
+struct ParsedArgs {
+    url: String,
+    method: String,
+    count: i32,
+}
