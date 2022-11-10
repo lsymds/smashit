ba7f55e224a4fbdc5166f7491c0b20e3ae4db998
Liam Symonds <liam@liamsymonds.com>
2022-11-10 22:54:54 +0000

    wip: make the request, start converting to async

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::ops::Add;
+use std::{
+    error::Error,
+    ops::Add,
+    time::{Duration, Instant},
+};
+
+use reqwest::StatusCode;
 
 fn main() {
     if let Some(parsed_args) = parse_args(std::env::args().collect()) {
@@ -7,10 +13,32 @@ fn main() {
             return;
         }
 
-        println!(
-            "URL is: {0} - Method is: {1} - Count is {2}",
-            parsed_args.url, parsed_args.method, parsed_args.count
-        );
+        println!("\nsmashit - a simple, single machine, CLI-based HTTP load testing tool built whilst learning rust\n");
+
+        let client = reqwest::blocking::Client::new();
+
+        // Asynchronously distribute all requests.
+        // Collate the responses.
+        // Return the summary of all responses, including the number of those that failed and their response code.
+
+        println!("🪄 Request summary");
+        println!("\tURL: {0}", parsed_args.url);
+        println!("\tMethod: {0}", parsed_args.method);
+        println!("\tCount: {0}\n", parsed_args.count);
+
+        match perform_request(&client, &parsed_args) {
+            RequestStastics::Success(response) => {
+                println!("😀 Request succeeded");
+                println!("\tStatus Code: {0}", response.status_code.to_string());
+                println!("\tResponse Size: {0}", response.response_size);
+                println!("\tLatency: {0}ms", response.latency.as_millis());
+                println!(
+                    "\tResponse Time: {0}ms",
+                    response.total_response_duration.as_millis()
+                );
+            }
+            _ => println!("💀 Request failed"),
+        }
     } else {
         show_help();
     }
@@ -36,7 +64,7 @@ fn parse_args(args: Vec<String>) -> Option<ParsedArgs> {
 
     return Some(ParsedArgs {
         url: path,
-        method,
+        method: method,
         count,
     });
 }
@@ -60,6 +88,8 @@ smashit - a simple, single machine, CLI-based HTTP load testing tool built whils
 
 usage: smashit [options]
 
+example: smashit -u https://my-api.com/users -c 25
+
 options:
   -c | --count  The number of times to call the endpoint (default: 1)
   -u | --url    The URL to load test
@@ -72,4 +102,42 @@ struct ParsedArgs {
     url: String,
     method: String,
     count: i32,
+}
+
+/// SuccessfulRequestStatistics represents timings, status codes and more pulled out from a successful request response.
+struct SuccessfulRequestStatistics {
+    status_code: StatusCode,
+    response_size: usize,
+    latency: Duration,
+    total_response_duration: Duration,
+}
+
+/// SuccessfulRequestStatistics represents timings, status codes and more pulled out from a failed request response.
+struct UnsuccessfulRequestStatistics {
+    status_code: StatusCode,
+}
+
+enum RequestStastics {
+    Success(SuccessfulRequestStatistics),
+    Failure(UnsuccessfulRequestStatistics),
+}
+
+/// perform_request performs the request for a given set of arguments parsed from the command line.
+fn perform_request(
+    client: &reqwest::blocking::Client,
+    parsed_args: &ParsedArgs,
+) -> RequestStastics {
+    let before_request = Instant::now();
+    let result = client.get(&parsed_args.url).send()?;
+    let latency = before_request.elapsed();
+    let status = result.status();
+    let response_size = result.bytes()?.len();
+    let total_response_duration = before_request.elapsed();
+
+    return Ok::<SuccessfulRequestStatistics, reqwest::Error>(SuccessfulRequestStatistics {
+        status_code: status,
+        response_size: response_size,
+        latency: latency,
+        total_response_duration,
+    });
 }
