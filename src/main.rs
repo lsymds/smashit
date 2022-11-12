5464cc99095979242bea634f7f8344a93ffe844d
Liam Symonds <liam@liamsymonds.com>
2022-11-12 21:22:05 +0000

    feat: add body documentation to help text

--- src/main.rs
+++ src/main.rs
@@ -152,7 +152,8 @@ options:
   -c | --count  The number of times to call the endpoint (default: 1)
   -u | --url    The URL to load test
   -m | --method The HTTP method to use in the request (default: GET)
-  -h | --header A header key value pair specified in the format of KEY=VALUE to be sent in the request"
+  -h | --header A header key value pair specified in the format of KEY=VALUE to be sent in the request
+  -b | --body   Text to send as part of the request's body."
     );
 }
 
